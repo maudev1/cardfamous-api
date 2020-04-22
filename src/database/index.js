7ca91f2229198aaa3a46e504c7fa57d24c78724f
Mauricio <mauriciojr.dev@gmail.com>
2020-04-21 22:48:07 -0300

    change bd
    - change dev-test  local  database for a hosted by umbler.com
    - finish sequelize configuration

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const dbConfig = require('../config/OLD')
+const dbConfig = require('../config/database')
 
 const connection = new Sequelize(dbConfig);
 
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json())
+
 app.use(routes);
 
 
